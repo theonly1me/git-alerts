b797692977211d0abbe61a8510638b82fc3ed941
Atchyut Pulavarthi <atchyut@qawolf.com>
2024-11-18 15:16:41 +0530

    chore(@git-alerts/frontend): update favicon and login view

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Login } from '@/views/Login';
 
 function App() { 
-  return <div className="h-screen bg-gray-950">
+  return <div className="h-screen bg-black">
     <Login />
   </div>
   ;
--- apps/frontend/src/views/Login.tsx
+++ apps/frontend/src/views/Login.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { GitHubIcon } from "@/components/icons/GitHubIcon";
 import { authenticate } from "@/lib/auth";
+import GitAlertsCat from '../assets/git-alerts-cat.webp';
 
 export function Login() {
   const handleLogin = () => {
@@ -9,8 +10,9 @@ export function Login() {
 
   return (
     <div className="flex flex-col gap-4 justify-center items-center w-full h-full text-neutral-50">
+      <img src={GitAlertsCat} alt="The git alerts cat holding bells" className="max-w-52"/>
       <h1 className="text-3xl">Welcome to git alerts! 🔔</h1>
-      <p className="text-xl">Sign in to get started.</p>
+      <p className="text-lg max-w-64 text-center">Sign in to get started.</p>
       <Button variant="secondary" title="Sign In" className="text-neutral-950" onClick={handleLogin}>Sign In with GitHub <GitHubIcon/></Button>
     </div> 
   );
